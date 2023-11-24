b91dbd4ff22f9c60f5e5d730cd169024ca79e847
udvari1dona324 <udvari.donat@telekom.hu>
2023-11-24 16:14:08 +0100

    some refactoring at spawning logic

--- src/roles/spawn_roles/builder_logic.py
+++ src/roles/spawn_roles/builder_logic.py
@@ -1,3 +1,6 @@
+from src.defs import *
+
+
 def need_extra(context):
-    # TODO akkor építsünk, ha van construction site
-    return False
+    construction_sites = context.room.find(FIND_MY_CONSTRUCTION_SITES)
+    return len(construction_sites) > 0
--- src/worker/spawn_executor.py
+++ src/worker/spawn_executor.py
@@ -14,13 +14,28 @@ __pragma__('noalias', 'update')
 def do_spawn():
     for name in Object.keys(Game.spawns):
         spawn = Game.spawns[name]
-        if not spawn.spawning and spawn.room.energyCapacityAvailable >= 250:
-            context = {}  # TODO feltölteni általános adatokkal
+        energy_capacity = spawn.room.energyAvailable
+        if not spawn.spawning and energy_capacity >= 250:
+            context = {
+                'room': spawn.room
+            }
             for role_name in Object.keys(SPAWN_PLAN):
                 role = SPAWN_PLAN[role_name]
                 num_role_creeps = _.sum(Game.creeps, lambda c: c.pos.roomName == spawn.pos.roomName and
                                                                c.memory.role == role_name)
                 if num_role_creeps < role.min or role.logic(context):
-                    # TODO kiszámolni a költségét és max 4 szeresére felnövelni az alap tervet, ha belefér
+                    multiplier = calculate_multiplier(energy_capacity, role.base_body)
+                    body_list = []
+                    if multiplier > 1:
+                        body_list = role.base_body
+                        for i in range(min(multiplier, 4)):
+                            body_list = [*body_list, *role.base_body]
                     name = "{}{}".format(role_name, Game.time)
-                    spawn.spawnCreep(role.base_body, name, {'memory': {'role': role_name}})
+                    spawn.spawnCreep(body_list, name, {'memory': {'role': role_name}})
+
+
+def calculate_multiplier(capacity, body_parts):
+    cost = 0
+    for body_part in body_parts:
+        cost += BODYPART_COST[body_part]
+    return capacity // cost
