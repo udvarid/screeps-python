c063ce7268d24b116416a3eb0d54c45f4d6087d0
udvari1dona324 <udvari.donat@telekom.hu>
2023-11-24 16:15:19 +0100

    one comment only

--- src/roles/spawn_roles/builder_logic.py
+++ src/roles/spawn_roles/builder_logic.py
@@ -3,4 +3,5 @@ from src.defs import *
 
 def need_extra(context):
     construction_sites = context.room.find(FIND_MY_CONSTRUCTION_SITES)
+    # TODO itt azért megnézni, hogy van e már más builder, vagy akár a normál mapbe beletenni egy max változót
     return len(construction_sites) > 0
