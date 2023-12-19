3bfd62b3c8c7189b6ad7e1749d96e3c297658924
udvari1dona324 <udvari.donat@telekom.hu>
2023-12-19 08:31:12 +0100

    refresh scouting feature is ready

--- src/roles/spawn_roles/scouter_logic.py
+++ src/roles/spawn_roles/scouter_logic.py
@@ -18,12 +18,43 @@ def more_scouter_is_needed(context):
     if Memory.room_map is undefined or Memory.room_map[context.room.name] is undefined:
         return False
     room_state = Memory.room_map[context.room.name]
-    is_uncharted = room_state['neighbours']['up'] == "?" or \
-                   room_state['neighbours']['bottom'] == "?" or \
-                   room_state['neighbours']['right'] == "?" or \
-                   room_state['neighbours']['left'] == "?"
-    # TODO később itt lehet olyan, hogy kikeressük azon szobákat, melyeket nem mi birtoklunk és régen térképeztük fel
-    return is_uncharted
+    has_uncharted_neighbour = room_state['neighbours']['up'] == "?" or \
+                              room_state['neighbours']['bottom'] == "?" or \
+                              room_state['neighbours']['right'] == "?" or \
+                              room_state['neighbours']['left'] == "?"
+    neighbour_scouted_long_ago = get_neighbour_scouted_long_ago(context.room.name)
+    return has_uncharted_neighbour or neighbour_scouted_long_ago is not undefined
+
+
+def get_neighbour_scouted_long_ago(room_name):
+    room_state = Memory.room_map[room_name]
+    not_room = ["?", "NO"]
+    neighbours = []
+    if not not_room.includes(room_state['neighbours']['up']):
+        neighbours.append(('up', room_state['neighbours']['up']))
+    if not not_room.includes(room_state['neighbours']['bottom']):
+        neighbours.append(('bottom', room_state['neighbours']['bottom']))
+    if not not_room.includes(room_state['neighbours']['right']):
+        neighbours.append(('right', room_state['neighbours']['right']))
+    if not not_room.includes(room_state['neighbours']['left']):
+        neighbours.append(('left', room_state['neighbours']['left']))
+
+    if len(neighbours) > 0:
+        my_rooms = []
+        for room_name in Object.keys(Game.rooms):
+            room = Game.rooms[room_name]
+            spawns = room.find(FIND_MY_SPAWNS)
+            if len(spawns) > 0:
+                my_rooms.append(room_name)
+
+        for neigh in neighbours:
+            if any(neigh[1] == room for room in my_rooms):
+                continue
+            scout_time = Memory.room_map[neigh[1]]['time']
+            if Game.time - scout_time > 15000:
+                return neigh[0]
+
+    return undefined
 
 
 def give_aim(room_name):
@@ -40,5 +71,7 @@ def give_aim(room_name):
         return "right"
     elif left == "?":
         return "left"
-    # TODO később itt lehet olyan, hogy kikeressük azon szobákat, melyeket nem mi birtoklunk és régen térképeztük fel
+    neighbour_scouted_long_ago = get_neighbour_scouted_long_ago(room_name)
+    if neighbour_scouted_long_ago is not undefined:
+        return neighbour_scouted_long_ago
     return "NA"
